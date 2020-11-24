b5dabde5239d0e3c175f0d5be81e97718a4baf9d
Felipe Araujo <ofc.felipearaujo@gmail.com>
2020-11-23 21:56:26 -0300

    configuração do models.py
    ainda incopleto

--- sas_esamaz/main01/models.py
+++ sas_esamaz/main01/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    ID = models.CharField(auto_created=True ,max_length=6, primary_key=True)
+    NOME = models.CharField(max_length=30)
+
+class Tipo_De_Sala(models.Model):
+    ID = models.CharField(max_length=30)
+    NOME = models.CharField(max_length=30)
+
+class Salas(models.Model):
+    CHOICE = [(1,'Ocupado'),(2,'Disponível')]
+    ID = models.CharField(auto_created=True ,max_length=6, primary_key=True)
+    NOME = models.CharField(max_length=30)
+    #FK_TIPO_SALAS = models.ForeignKey ( )
+    STATUS = models.BooleanField(choices=CHOICE,default=1, blank=False)
+
+class Periodo(models.Model):
+    ID = models.CharField(auto_created=True ,max_length=6, primary_key=True)
+    NOME = models.CharField(max_length=30)
+    #INICIO =
+    #TERMINO =
+
+class TabelaDeAgendamento (models.Model):
+    CHOICE = [(1,'Sim'), (2,'Não')]
+    ID = models.CharField(auto_created=True ,max_length=6, primary_key=True)
+    DATA_CRIACAO = models.DateTimeField(auto_now=True)
+    OBSERVAÇÃO = models.CharField(max_length=175)
+    #STATUS = BOOLEANO
+    #FK_SALAS = models.ForeignKey(Salas)
+    #FK_PERIODOS = models.ForeignKey(Periodo)
+    #FK_PROFESSOR = models.ForeignKey(Professor)
+    CAIXA_DE_SOM = models.BooleanField(choices=CHOICE,blank=True)
--- sas_esamaz/sas_esamaz/settings.py
+++ sas_esamaz/sas_esamaz/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main01'
 ]
 
 MIDDLEWARE = [
@@ -65,12 +66,16 @@ WSGI_APPLICATION = 'sas_esamaz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testedb', 
+        'USER': 'postgres', 
+        'PASSWORD': 'felipe',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
-}
+}'''
 
 
 # Password validation
